801643846945547d64e571a187fb388ad6f50f0f
nth-chile <jaredsalzano@gmail.com>
2018-08-20 21:11:21 -0400

    Add prop-types, create components folder

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var path = require("path"),
-	express = require("express");
+var path = require("path");
+var express = require("express");
+var app = express();
+var	http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 var DIST_DIR = path.join(__dirname, "dist"),
-    PORT = 3000,
-    app = express();
+    PORT = 3000;
 
 //Serving the files on the dist folder
 app.use(express.static(DIST_DIR));
@@ -13,4 +15,10 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(DIST_DIR, "index.html"));
 });
 
-app.listen(PORT);
+io.on("connection", function(socket) {
+	console.log('a user connected');
+});
+
+app.listen(PORT, function() {
+	console.log('listening on port 3000.')
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hi');
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<ul id="messages"></ul>
+			    <form action="">
+			      <input id="m" /><button>Send</button>
+			    </form>
+			</div>
+		);
+	}
+}
+
+render(<App />, window.document.getElementById("app"));
